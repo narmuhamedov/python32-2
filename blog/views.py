53cade34b5107098aa5a19bc57bd74e71a0a4327
rzpy1997 <kingradomir@gmail.com>
2023-09-11 21:38:10 +0600

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from . import models
+
+def program_lang_view(request):
+    lang = models.ProgramLang.objects.all()
+    return render(request, 'blog.html', {'lang_key': lang})
